dbe617fe0616893ec36fb3e2f441850e29fd626c
Lucas Delobelle <lucas@ld-web.net>
2024-05-07 11:52:48 +0200

    :wrench: Prepare for GH Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,23 +3,26 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ld-web.github.io",
+  base: "appendix",
   integrations: [
     starlight({
       title: {
         fr: "Annexes",
         en: "Appendix",
       },
-      defaultLocale: "fr",
       locales: {
-        fr: {
+        root: {
           label: "Français",
+          lang: "fr",
         },
         en: {
           label: "English",
+          lang: "en",
         },
       },
       social: {
-        github: "https://github.com/withastro/starlight",
+        github: "https://github.com/ld-web/appendix",
       },
     }),
   ],
